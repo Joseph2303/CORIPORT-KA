b170d8b198830b603c4471995d504de95fed6b42
Quintanilla1299 <suke12k17@gmail.com>
2024-05-28 22:39:43 -0600

    caso21

--- CORIPORT/app/Providers/AppServiceProvider.php
+++ CORIPORT/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+            // Verificar si el valor es una cadena base64 válida y representa una imagen
+            if (preg_match('/^data:image\/(\w+);base64,/', $value)) {
+                return true;
+            }
+            return false;
+        });
     }
 }
